672c03cb25cef4d561006f4000025bc86c06a0b7
Rafael Lisboa <eurafaellisboa@gmail.com>
2022-06-19 00:18:15 -0300

    Adiciona parcelamento e desconto a pagina de produtos e listagem de produtos

---
+++ Backendorf/Installment/README.md
@@ -0,0 +1,6 @@
+# Módulo de parcelamento para Magento 2
+### Detalhes -> [AQUI](https://github.com/DaviBackendorf/magento2-installment/wiki/Vis%C3%A3o-geral)
+
+
+# Installment module for Magento 2
+### Details -> [HERE](https://github.com/DaviBackendorf/magento2-installment/wiki/English-Overview)
---
+++ Backendorf/Installment/view/frontend/web/js/price-box-mixin.js
@@ -0,0 +1,47 @@
+/*
+ * price-box-mixin | Backendorf
+ *
+ * @category  Backendorf
+ * @package   price-box-mixin.js
+ *
+ * @copyright Copyright (c) 2021 Backendorf - Magento Developer.
+ *
+ * @author    Davi Backendorf <davijacksonb@gmail.com>
+ */
+
+define([
+    'jquery',
+    'Magento_Catalog/js/price-utils',
+    'underscore',
+    'mage/template',
+    'jquery/ui'
+], function ($, utils, _, mageTemplate) {
+    'use strict';
+
+    return function (widget) {
+        $.widget('mage.priceBox', widget,
+            {
+                reloadPrice: function reDrawPrices() {
+                    var priceFormat = (this.options.priceConfig && this.options.priceConfig.priceFormat) || {},
+                        priceTemplate = mageTemplate(this.options.priceTemplate);
+
+                    _.each(this.cache.displayPrices, function (price, priceCode) {
+                        price.final = _.reduce(price.adjustments, function (memo, amount) {
+                            return memo + amount;
+                        }, price.amount);
+
+                        price.formatted = utils.formatPrice(price.final, priceFormat);
+
+                        $('[data-price-type="' + priceCode + '"]', this.element).html(priceTemplate({
+                            data: price
+                        }));
+                        if (priceCode == 'finalPrice') {
+                            let element = this.element;
+                            $('body').trigger('afterReloadPrice', {price, element});
+                        }
+                    }, this);
+                }
+            });
+        return $.mage.priceBox;
+    }
+});
