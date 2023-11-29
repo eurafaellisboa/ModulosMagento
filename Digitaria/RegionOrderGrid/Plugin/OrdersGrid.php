945a01135e8ab8b3df1301e2f65bcea09356a77e
Rafael Lisboa <eurafaellisboa@gmail.com>
2023-11-29 08:28:31 -0300

    Adiciona o Estado (UF) no Grid de Pedidos do ADMIN

---
+++ Digitaria/RegionOrderGrid/Plugin/OrdersGrid.php
@@ -0,0 +1,23 @@
+<?php
+namespace Digitaria\RegionOrderGrid\Plugin;
+
+class OrdersGrid
+{
+    public function afterGetReport($subject, $collection, $requestName)
+    {
+        if ($requestName !== 'sales_order_grid_data_source') {
+            return $collection;
+        }
+
+        if ($collection->getMainTable() === $collection->getResource()->getTable('sales_order_grid')) {
+            $orderAddressTable = $collection->getResource()->getTable('sales_order_address');
+            $collection->getSelect()->joinLeft(
+                ['oat' => $orderAddressTable],
+                'oat.parent_id = main_table.entity_id AND oat.address_type = \'shipping\'',
+                ['region']
+            );
+        }
+
+        return $collection;
+    }
+}
